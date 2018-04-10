70f1d3e7d78f12c35753a3a134a33c135b25023a
Matthew Cochran <matthewjcochran@gmail.com>
2018-04-09 21:27:04 -1000

    exercise 3

--- exercises.js
+++ exercises.js
@@ -53,6 +53,20 @@ This function will iterate through the person parameter and console.log the foll
 */
 
 
+var presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monro'];
+
+for (var i = 0; i < presidents.length; i++){
+  console.log('The value at ' + i + ' is ' + presidents[i] + '.');
+}
+
+function leaders(person){
+  for(var i = 0; i < person.length; i++){
+    console.log('President ' + person[i] + ' was a great leader.');
+  }
+}
+
+leaders(presidents);
+
 /* 4) Line Number
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
 

ef7ebbafae6478c8a8f9f29187fa8055ea4b6bcb
Matthew Cochran <matthewjcochran@gmail.com>
2018-04-10 09:25:27 -1000

    exercise 9

--- exercises.js
+++ exercises.js
@@ -145,7 +145,6 @@ for(var i = 0; i < oopsArray.length; i++){
   console.log(oopsArray[oopsArray.length- 1 - i]);
 }
 
-
 /* 9) Siesta Time
 Declare a variable named `napSchedule` and assign its value to the following array: `[false, false, true, false, true, true]`
 
@@ -156,7 +155,19 @@ Next, write a function named `nap`. This function takes in a single parameter: `
 Inside of this function write a for-loop that will iterate through the `napSchedule` array and console.log the message: `ZzZzZzZz` if the schedule is `true`, otherwise the it will console.log the message: `Gotta get coding!` if the schedule is `false`.
 */
 
+var napSchedule = [false, false, true, false, true, true];
 
+function nap(schedule){
+  for(var i = 0; i < schedule.length; i++){
+    if(schedule[i] === true){
+      console.log('ZzZzZzZz');
+    }else{
+      console.log('Gotta get coding!');
+    }
+  }
+}
+
+nap(napSchedule);
 
 /* 10) Copy Pasta
 Declare a variable named `valuesArray` and assign its value to be an array: `[99, 66, 829, 1941, 8, 76]`. 
